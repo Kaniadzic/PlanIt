57c27551cd1ee2ec223a87445c1ffbcd0fb8a2d3
Kaniadzic <szymon.kania@edu.uekat.pl>
2023-06-07 18:22:31 +0200

    Functionality: workspaces list and fixes

--- app/src/main/java/com/example/planit/WorkspacesActivity.kt
+++ app/src/main/java/com/example/planit/WorkspacesActivity.kt
@@ -2,15 +2,21 @@ package com.example.planit
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.planit.databinding.ActivityHomepageBinding
 import com.example.planit.databinding.ActivityWorkspacesBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class WorkspacesActivity : AppCompatActivity() {
     private lateinit var binding: ActivityWorkspacesBinding
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var databaseReference: DatabaseReference
+    private val workspacesList = mutableListOf<Workspace?>()
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -43,5 +49,65 @@ class WorkspacesActivity : AppCompatActivity() {
         binding.btnAddWorkspace.setOnClickListener(View.OnClickListener {
             startActivity(Intent(applicationContext, AddWorkspaceActivity::class.java))
         })
+
+        getWorkspaces()
+    }
+
+    fun getWorkspaces() {
+        databaseReference = FirebaseDatabase
+            .getInstance("https://planit-79310-default-rtdb.europe-west1.firebasedatabase.app/")
+            .getReference("Workspaces")
+
+        val userID = FirebaseAuth.getInstance().currentUser?.uid.toString()
+        var dataSnapshot: DataSnapshot
+
+        databaseReference
+            .get()
+            .addOnSuccessListener{
+                dataSnapshot = it
+
+                dataSnapshot.children.forEach{
+                    workspacesList.add(it.getValue(Workspace::class.java))
+                }
+
+                var workspacesNames = arrayOf<String?>()
+                var workspacesTypes = arrayOf<String?>()
+                var workspacesIDs = arrayOf<String?>()
+
+                workspacesList.forEach{
+                    workspacesNames = workspacesNames.plus(it?.name)
+                    workspacesTypes = workspacesTypes.plus(it?.type)
+                    workspacesIDs = workspacesIDs.plus(it?.id)
+                }
+
+                val workspacesListAdapter = WorkspacesListAdapter(this, workspacesNames, workspacesTypes, workspacesIDs)
+                binding.lvWorkspaces.adapter = workspacesListAdapter
+
+                binding.lvWorkspaces.setOnItemClickListener(){adapterView, view, position, id ->
+                    val itemAtPos = adapterView.getItemAtPosition(position)
+                    Toast.makeText(this, "$itemAtPos", Toast.LENGTH_LONG).show()
+                }
+
+                Toast.makeText(this, "Sukces!", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener{
+                Toast.makeText(this, "Jebać ruch!!", Toast.LENGTH_SHORT).show()
+            }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/planit/WorkspacesListAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.planit
+
+import android.app.Activity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+
+class WorkspacesListAdapter (
+    private val context: Activity,
+    private val name: Array<String?>,
+    private val type: Array<String?>,
+    private val workspaceIDs: Array<String?>
+    )
+    : ArrayAdapter<String>(context, R.layout.list_workspaces, workspaceIDs) {
+
+    override fun getView(position: Int, view: View?, parent: ViewGroup): View {
+        val inflater = context.layoutInflater
+        val rowView = inflater.inflate(R.layout.list_workspaces, null, true)
+
+        val titleText = rowView.findViewById(R.id.name) as TextView
+        val subtitleText = rowView.findViewById(R.id.type) as TextView
+        val id = rowView.findViewById(R.id.id) as TextView
+
+        titleText.text = name[position]
+        subtitleText.text = type[position]
+        id.text = workspaceIDs[position]
+
+        return rowView
+    }
+}
